139ce1da63bc1ba9adbf3147bd5a2453e8c3cb74
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2023-01-20 10:02:39 -0500

    Installed react-navigation and dependencies, wrapped our app.js, configured a stack navigator

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,13 +2,20 @@ import { FlatList } from 'react-native';
 import CategoryGridTile from '../components/CategoryGridTile';
 import { CATEGORIES } from '../data/dummy-data';
 
-function renderCategoryItem(itemData) {
-  return (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-  );
-}
+function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate('MealsOverview');
+    }
 
-function CategoriesScreen() {
+    return (
+      <CategoryGridTile
+        onPress={pressHandler}
+        title={itemData.item.title}
+        color={itemData.item.color}
+      />
+    );
+  }
   return (
     <FlatList
       data={CATEGORIES}
