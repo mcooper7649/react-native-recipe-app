16cccbc0b5bceb15ed52b2fe43d1a321de548a1f
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2023-01-20 13:00:18 -0500

    styled MealItem Component and added pressable animations

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,34 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { FlatList, View, StyleSheet, Text } from 'react-native';
+import MealItem from '../components/MealItem';
 import { MEALS } from '../data/dummy-data';
 
 export default function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
+
+  const displayMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      duration: item.duration,
+      complexity: item.complexity,
+    };
+
+    return <MealItem {...mealItemProps} />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
